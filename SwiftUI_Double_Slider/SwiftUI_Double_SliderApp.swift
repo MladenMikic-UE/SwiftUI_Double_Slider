7002eb12fa888825a4d5aa5cbc631f8bb86bda78
Mladen Mikic <mladen.mikic@ignit.group>
2023-07-19 09:58:33 +0200

    Initial Commit

---
+++ SwiftUI_Double_Slider/SwiftUI_Double_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Double_SliderApp.swift
+//  SwiftUI_Double_Slider
+//
+//  Created by MladenMikic on 19.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Double_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
